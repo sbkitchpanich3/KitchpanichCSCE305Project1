b93afd727ca2e8df637761abf07a01eb8154849e
sbkitchpanich3 <bankster007@gmail.com>
2017-08-30 02:10:54 -0800

    Initial commit

---
+++ app/src/main/java/com/sbkitchpanich/kitchpanichcsce305project1/PageOne.java
@@ -0,0 +1,13 @@
+package com.sbkitchpanich.kitchpanichcsce305project1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PageOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_one);
+    }
+}
