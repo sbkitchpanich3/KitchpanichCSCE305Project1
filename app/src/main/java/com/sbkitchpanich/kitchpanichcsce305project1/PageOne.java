3ea90bd557966bd19684c756743396b4310f7048
sbkitchpanich3 <bankster007@gmail.com>
2017-08-30 23:51:52 -0800

    got two pages working

--- app/src/main/java/com/sbkitchpanich/kitchpanichcsce305project1/PageOne.java
+++ app/src/main/java/com/sbkitchpanich/kitchpanichcsce305project1/PageOne.java
@@ -1,7 +1,9 @@
 package com.sbkitchpanich.kitchpanichcsce305project1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PageOne extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class PageOne extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_one);
+    }
+
+    public void onToPageTwo(View view) {
+        Intent getPageTwoIntent = new Intent(this, PageTwo.class);
+
+        //final int result = 1;
+
+        startActivity(getPageTwoIntent);
     }
 }
---
+++ app/src/main/java/com/sbkitchpanich/kitchpanichcsce305project1/PageTwo.java
@@ -0,0 +1,16 @@
+package com.sbkitchpanich.kitchpanichcsce305project1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class PageTwo extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.pagetwo);
+    }
+}
